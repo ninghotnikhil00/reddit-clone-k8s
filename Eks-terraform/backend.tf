d74fcb129710f85538db420f457e320f1ea30ec2
nikhil <158544808+ninghotnikhil00@users.noreply.github.com>
2024-09-30 15:56:51 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "bucketoftheyear" # Replace with your actual S3 bucket name
+    bucket = "cicdbucketofnikhil" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-southeast-1"
   }
